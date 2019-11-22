b058e0f8c56e9b1f68dadb53b3a657ee761e4a87
eubans <markivan.eubans@hilsoftinc.com>
2019-11-22 14:45:51 +0800

    initial commit

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\NewMessage;
+use Illuminate\Http\Request;
+use App\User;
+use App\Message;
+
+class ContactsController extends Controller
+{
+    public function get()
+    {
+        $contacts = User::where('id', '!=', auth()->id())->get();
+
+        $unreadIds = Message::select(\DB::raw('`from` as sender_id, count(`from`) as message_count'))
+            ->where('to', auth()->id())
+            ->where('read', false)
+            ->groupBy('from')
+            ->get();
+
+        $contacts = $contacts->map(function ($contact) use ($unreadIds) {
+            $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
+
+            $contact->unread = $contactUnread ? $contactUnread->message_count : 0;
+
+            return $contact;
+        });
+
+        return response()->json($contacts);
+    }
+
+    public function getMessagesFor($id)
+    {
+        Message::where('from', $id)->where('to', auth()->id())->update(['read' => true]);
+
+        $messages = Message::where(function ($q) use ($id) {
+            $q->where('from', auth()->id());
+            $q->where('to', $id);
+        })->orWhere(function ($q) use ($id) {
+            $q->where('from', $id);
+            $q->where('to', auth()->id());
+        })->get();
+
+        return response()->json($messages);
+    }
+
+    public function send(Request $request)
+    {
+        $message = Message::create([
+            'from' => auth()->id(),
+            'to' => $request->contact_id,
+            'text' => $request->text
+        ]);
+
+        broadcast(new NewMessage($message));
+
+        return response()->json($message);
+    }
+}
